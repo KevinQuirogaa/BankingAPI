47dc642e4a3a78746c66b63cbba6679ce5c0faba
KevinQuirogaa <kgomezquiroga2@gmail.com>
2025-04-17 08:50:30 -0500

    Estructura inicial del proyecto BankingAPI utilizando arquitectura N Capas

---
+++ BankingAPI.Modules.Logic/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingAPI.Modules.Logic
+{
+    internal class DependencyInjection
+    {
+    }
+}
