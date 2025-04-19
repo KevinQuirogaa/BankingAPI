7bc823e7d30ef65d2ce658e13567e3581c0a787f
KevinQuirogaa <kgomezquiroga2@gmail.com>
2025-04-19 08:19:57 -0500

    Refactorización de modelos y configuración de mapeo
    Se eliminó la carpeta "Models" del proyecto `BankingAPI.Modules.BO.csproj` y se añadieron las clases `AccountModel`, `CustomerModel` y `TransactionModel` para representar cuentas, clientes y transacciones.
    
    En `BankingAPI.Modules.Infrastructure.csproj`, se eliminaron referencias a la carpeta "Mapping" y se añadieron paquetes como `AutoMapper` y `Entity Framework Core` para mejorar el mapeo y acceso a datos.
    
    Se estableció una referencia al proyecto `BankingAPI.Modules.BO` y se creó un nuevo perfil de mapeo en `MappingProfile.cs` para facilitar la conversión entre entidades y modelos.
    
    Finalmente, se registró `AutoMapper` en el contenedor de servicios en `Program.cs`, mejorando la organización y reutilización del código.

---
+++ BankingAPI.Modules.BO/Models/AccountModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BankingAPI.Modules.BO.Models
+{
+    public class AccountModel
+    {
+        public string AccountNumber { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public string AccountType { get; set; }
+
+        public decimal Balance { get; set; }
+
+    }
+}
--- BankingAPI/Program.cs
+++ BankingAPI/Program.cs
@@ -13,6 +13,10 @@ builder.Services.AddSwaggerGen();
 // Inyectar las capas de la aplicacion
 builder.Services.AddDependencyI(builder.Configuration);
 
+// Registra el AutoMapper en la aplicacion utilizando el Profile de un archivo en especifico
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddAutoMapper(typeof(BankingAPI.Modules.Infrastructure.Mapping.MappingProfile));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
